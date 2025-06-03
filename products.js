90a013e6fdeaff358445a8a461a96f531715874e
Oli Jackson <ogjackson@student.unimelb.edu.au>
2025-06-04 09:14:32 +1000

    Added product information in JS

---
+++ products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        ID: 'recliner1',
+        Type: '',
+        Name: '',
+        Description: '',
+        Price: '',
+        Sizes: '',
+        Materials: ''
+    },
+    {
+        ID: 'recliner1',
+        Type: '',
+        Name: '',
+        Description: '',
+        Price: '',
+        Sizes: '',
+        Materials: ''
+    },
+    {
+        ID: 'recliner1',
+        Type: '',
+        Name: '',
+        Description: '',
+        Price: '',
+        Sizes: '',
+        Materials: ''
+    }
+]
