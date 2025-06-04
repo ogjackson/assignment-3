0c323773f51c59eb4b6b7a4471f7e7831a318796
Oli Jackson <ogjackson@student.unimelb.edu.au>
2025-06-04 23:06:54 +1000

    Added global JS for header and product pages.

---
+++ header.js
@@ -0,0 +1,18 @@
+const header = `
+    <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Sale</li>
+        <li>About</li>
+     </ul>
+
+    <a class="logo">proudfurniture</a>
+
+    <div>
+        <img class="icon" src="assets/common/search-black.svg"></img>
+        <img class="icon" src="assets/common/cart-black.svg"></img>
+     </div>
+
+    `
+
+document.getElementById('header').innerHTML = header;
--- products.js
+++ products.js
@@ -1,29 +1,21 @@
 const products = [
     {
-        ID: 'recliner1',
-        Type: '',
-        Name: '',
-        Description: '',
-        Price: '',
-        Sizes: '',
-        Materials: ''
+        id: 'recliner1',
+        type: '',
+        name: 'recliner',
+        image: '/assets/placeholder.jpg',
+        description: 'lorem ipsum',
+        price: '100 dollary doos',
+        sizes: '',
+        materials: ''
     },
-    {
-        ID: 'recliner1',
-        Type: '',
-        Name: '',
-        Description: '',
-        Price: '',
-        Sizes: '',
-        Materials: ''
-    },
-    {
-        ID: 'recliner1',
-        Type: '',
-        Name: '',
-        Description: '',
-        Price: '',
-        Sizes: '',
-        Materials: ''
-    }
 ]
+
+const product = products[0];
+
+
+document.getElementById("name").textContent = product.name;
+document.getElementById("image").src = product.image;
+document.getElementById("image").alt = product.name;
+document.getElementById("description").textContent = product.description;
+document.getElementById("price").textContent = product.price;
